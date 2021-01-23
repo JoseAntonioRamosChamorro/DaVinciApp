7798e540de057748f106638a7331c97fbe3139e4
joseanton <joseantonioramoschamorro@gmail.com>
2021-01-23 19:24:01 +0100

    Primer commit

--- app/src/main/java/com/example/davinciapp/MainActivity.java
+++ app/src/main/java/com/example/davinciapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.davinciapp;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -26,27 +30,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navpintor, R.id.navescul, R.id.navcient, R.id.fragmentInventor)
+                R.id.navpintor, R.id.navescul, R.id.navcient, R.id.inventorFrag)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -60,4 +55,24 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.altaaction:
+                        Toast.makeText(this, "Dando alta", Toast.LENGTH_SHORT).show();
+                        break;
+            case R.id.bajaaction:
+                Toast.makeText(this, "Dando baja", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.modiaction:
+                Toast.makeText(this, "Modificando ", Toast.LENGTH_SHORT).show();
+                break;
+                case R.id.consulaction:
+                Toast.makeText(this, "Consultando ", Toast.LENGTH_SHORT).show();
+                    break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
